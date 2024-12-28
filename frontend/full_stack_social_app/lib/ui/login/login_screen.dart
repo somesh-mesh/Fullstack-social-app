8ccc1d57cfe9d2e041eee772b594c974b9ad9543
somesh-mesh <somesh-mesh>
2024-12-29 00:34:55 +0530

    icons added and ui

---
+++ frontend/full_stack_social_app/lib/ui/login/login_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Color(0xFFF2F2F2),
+      body: Center(
+        child: Text("Hello"),
+      ),
+    );
+  }
+}
